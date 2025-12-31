2a11b53a6cf14e0c53827f46f4116ef97bf1b1da
Van5sh <vansh05dhir@gmail.com>
2025-12-31 15:06:17 +0530

    feat:expense_repository_setup

---
+++ internal/repository/expense_repository.go
@@ -0,0 +1,70 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"strconv"
+
+	sqlc "github.com/Van5sh/new-splitwise/internal/db/sqlc"
+	"github.com/google/uuid"
+)
+
+type ExpenseRepository struct {
+	q *sqlc.Queries
+}
+
+func NewExpenseRepository(q *sqlc.Queries) *ExpenseRepository {
+	return &ExpenseRepository{q: q}
+}
+
+func (r *ExpenseRepository) GetExpenses(ctx context.Context) ([]sqlc.Expense, error) {
+	return r.q.GetExpenses(ctx)
+}
+
+func (r *ExpenseRepository) GetExpenseByID(ctx context.Context, id string) (sqlc.Expense, error) {
+	ExpenseId, err := uuid.Parse(id)
+	if err != nil {
+		return sqlc.Expense{}, err
+	}
+	return r.q.GetExpenseById(ctx, ExpenseId)
+}
+
+func (r *ExpenseRepository) GetExpenseByGroupID(ctx context.Context, id string) ([]sqlc.Expense, error) {
+	groupId, err := uuid.Parse(id)
+	if err != nil {
+		return nil, err
+	}
+	return r.q.GetExpensesByGroupId(ctx, groupId)
+}
+
+func (r *ExpenseRepository) GetExpenseByUserId(ctx context.Context, id string) ([]sqlc.Expense, error) {
+	userId, err := uuid.Parse(id)
+	if err != nil {
+		return nil, err
+	}
+	return r.q.GetExpensesByUserId(ctx, userId)
+}
+
+func (r *ExpenseRepository) CreateExpense(ctx context.Context, groupId, userId, description string,
+	amount int) (sqlc.Expense, error) {
+	groupUUID, err := uuid.Parse(groupId)
+	if err != nil {
+		return sqlc.Expense{}, err
+	}
+
+	_ = userId
+
+	return r.q.CreateExpense(ctx, sqlc.CreateExpenseParams{
+		GroupID:     groupUUID,
+		Description: sql.NullString{String: description, Valid: description != ""},
+		Amount:      strconv.Itoa(amount),
+	})
+}
+
+func (r *ExpenseRepository) DeleteExpense(ctx context.Context, id string) (sqlc.Expense, error) {
+	expenseId, err := uuid.Parse(id)
+	if err != nil {
+		return sqlc.Expense{}, err
+	}
+	return r.q.DeleteExpense(ctx, expenseId)
+}
