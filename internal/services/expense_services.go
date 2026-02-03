7c207b280d947626be4cae8fee70094877b7ef11
Van5sh <vansh05dhir@gmail.com>
2026-02-03 12:22:59 +0530

    expenses added

--- internal/services/expense_services.go
+++ internal/services/expense_services.go
@@ -1,1 +1,13 @@
 package services
+
+import (
+	domain "github.com/Van5sh/new-splitwise/domain/repositories"
+)
+
+type ExpenseServices struct {
+	repo domain.ExpenseRepository
+}
+
+func NewExpenseServices(repo domain.ExpenseRepository) *ExpenseServices {
+	return &ExpenseServices{repo: repo}
+}
