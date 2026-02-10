acb0e9df2f7ad30b1c8870562be3256070b2f986
Van5sh <vansh05dhir@gmail.com>
2026-02-10 18:47:36 +0530

    router fixes done

--- internal/router/balance_router.go
+++ internal/router/balance_router.go
@@ -1,11 +1,18 @@
 package router
 
-import "github.com/Van5sh/new-splitwise/internal/handler"
+import (
+	"github.com/Van5sh/new-splitwise/internal/handler"
+	"github.com/gofiber/fiber/v2"
+)
 
 type BalanceRouter struct {
 	handler *handler.BalanceHandler
 }
 
 func NewBalanceRouter(handler *handler.BalanceHandler) *BalanceRouter {
-	return {handler:handler}
+	return &BalanceRouter{handler: handler}
+}
+
+func (r *BalanceRouter) BalanceRouters(app *fiber.App) {
+
 }
